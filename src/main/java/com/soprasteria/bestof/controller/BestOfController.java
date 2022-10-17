b1ac10cf56c18d7a3930d7dca63400c8faa9d884
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-17 16:06:09 +0200

    effettua una GET che restituisce la miglior canzone del 2022

--- src/main/java/com/soprasteria/bestof/controller/BestOfController.java
+++ src/main/java/com/soprasteria/bestof/controller/BestOfController.java
@@ -8,17 +8,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.soprasteria.bestof.model.Movie;
+import com.soprasteria.bestof.model.Song;
 
 @RestController
 @RequestMapping("/bestof")
 public class BestOfController {
 	
 	@GetMapping("/movie")
-	public Movie movie() {
+	public Movie bestMovieOf2022() {
 		List<String> cast = new ArrayList<String>();
 		cast.add("A");
 		cast.add("B");
 		return new Movie("Film", 2022, "Mr.",cast);
+	}
+	
+	@GetMapping("/song")
+	public Song bestSongOf2022() {
+		return new Song("Metal", 2022, "Metal", "A", "B");
 	}
 
 }
