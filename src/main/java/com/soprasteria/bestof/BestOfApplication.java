51d502080933daa39e9d944030b64658520199e6
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-17 15:28:48 +0200

    the best of

---
+++ src/main/java/com/soprasteria/bestof/BestOfApplication.java
@@ -0,0 +1,13 @@
+package com.soprasteria.bestof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestOfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestOfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soprasteria/bestof/BestOfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soprasteria.bestof;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestOfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
